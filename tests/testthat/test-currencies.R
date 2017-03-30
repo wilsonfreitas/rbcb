6169c2a548dc8251fb0cd504dfb05d9fa5ea8d87
wilsonfreitas <wilson.freitas@gmail.com>
2017-03-30 08:05:17 -0300

    Updated tests

--- tests/testthat/test-currencies.R
+++ tests/testthat/test-currencies.R
@@ -1,6 +1,12 @@
 
 context("currencies")
 
+test_that("it should get currency data from BCB", {
+  x <- get_currency("USD", "2017-03-01", "2017-03-02", as = "text")
+  expect_is(x, "character")
+  expect_equal(x, "01032017;220;A;USD;3,0970;3,0976;1,0000;1,0000\n02032017;220;A;USD;3,1132;3,1138;1,0000;1,0000\n")
+})
+
 test_that("it should get a time series of a currency from bcb", {
   x <- get_currency("USD", "2017-03-01", "2017-03-14")
   expect_is(x, "data.frame")
---
+++ tests/testthat/test-get_series.R
@@ -0,0 +1,37 @@
+context("get_series")
+
+test_that("it should get series json", {
+  x <- get_series(1, start_date = "2017-03-01", end_date = "2017-03-01", as = "text")
+  expect_is(x, "character")
+  expect_true(jsonlite::validate(x))
+  expect_equal(x, '[{"data":"01/03/2017","valor":"3.0976"}]')
+})
+
+test_that("it should get one series as data.frame", {
+  x <- get_series(1, last = 10)
+  expect_equal(dim(x)[1], 10)
+  expect_equal(dim(x)[2], 2)
+  expect_true(!anyNA(x[,1]))
+  expect_true(!anyNA(x[,2]))
+  expect_is(x, "data.frame")
+  expect_is(x$date, "POSIXct")
+  expect_is(x$`1`, "numeric")
+})
+
+test_that("it should name the series", {
+  x <- get_series(1, last = 10, name = "USD")
+  expect_equal(colnames(x), c("date", "USD"))
+})
+
+test_that("it should get series as xts", {
+  x <- get_series(1, last = 10, name = "USD", as = "xts")
+  expect_equal(colnames(x), "USD")
+})
+
+test_that("it should get series within a date period", {
+  x <- get_series(1, start_date = "2017-03-01", end_date = "2017-03-29", name = "USD")
+  expect_is(x$date, "POSIXct")
+  expect_true(x$date[1] == "2017-03-01")
+  expect_true(x$date[dim(x)[1]] == "2017-03-29")
+})
+
