4b7bea0cd58da381603b4efe0c93f45fcb7d8185
wilsonfreitas <wilson.freitas@gmail.com>
2018-03-01 07:46:02 -0300

    Changed series_info to be a method of series_obj

--- R/series_info.R
+++ R/series_info.R
@@ -1,17 +1,33 @@
 
-series_info_url = function(code) {
-  sprintf("https://www3.bcb.gov.br/sgspub/consultarvalores/consultarValoresSeries.do?method=consultarGraficoPorId&hdOidSeriesSelecionadas=%d", code)
+series_info_url = function(x) UseMethod("series_info_url")
+
+series_info_url.series_obj = function(x) {
+  url = "https://www3.bcb.gov.br/sgspub/consultarvalores/consultarValoresSeries.do?method=consultarGraficoPorId"
+  httr::modify_url(url, query = list(hdOidSeriesSelecionadas = x$code))
 }
 
-series_info = function(code) {
-  url = series_info_url(code)
+series_info = function(x) UseMethod("series_info")
+
+series_info.series_obj = function(x) {
+  url = series_info_url(x)
   res = httr::GET(url)
   cnt = httr::content(res, as = "text")
+
   doc = xml2::read_html(cnt)
   info = xml2::xml_find_first(doc, '//tr[@class="fundoPadraoAClaro3"]')
   info = xml2::xml_find_all(info, ".//td")
   info = xml2::xml_text(info)
-  info = info[-length(info)]
-  setNames(info, c("code", "description", "unit", "frequency", "start_date", "last_date"))
+  info = as.list(info[-length(info)])
+  info = setNames(info, c("code", "description", "unit", "frequency", "start_date", "last_date"))
+
+  if (as.numeric(info$code) != x$code)
+    stop("Downloaded info is different from series info")
+
+  info$code = NULL
+  info$url = url
+  info$start_date = as.Date(info$start_date, "%d/%m/%Y")
+  info$last_date = as.Date(info$last_date, "%d/%m/%Y")
+
+  info
 }
 
