c7963c5ac4833c8467ed88953cd555c099f7c607
wilsonfreitas <wilson.freitas@gmail.com>
2018-06-27 07:59:44 -0300

    Implemented function to get inflation's expectations

--- R/rbcb_get_market_expectations.R
+++ R/rbcb_get_market_expectations.R
@@ -276,3 +276,88 @@ annual_market_expectations_url <- function(indic, start_date, end_date, ...) {
                                 `$select` = "Indicador,IndicadorDetalhe,Data,DataReferencia,Media,Mediana,DesvioPadrao,CoeficienteVariacao,Minimo,Maximo", ...))
 }
 
+#' Get inflation's market expectations for the next 12 months
+#'
+#' Statistics of inflation's market expectations for the next 12 months:
+#' mean, median, standard
+#' deviate, minimum, maximum and the coefficient of variation.
+#' All statistics are computed based on expectations provided by many financial
+#' institutions in Brazil: banks, funds, risk managers, so on and so forth.
+#' These expections and its statistics are used to build the FOCUS Report weekly
+#' released by the Brazilian Central Bank.
+#'
+#' There are inflation's expectations available for the following indicators:
+#' IGP-DI, IGP-M, INPC, IPA-DI, IPA-M, IPCA, IPCA-15, IPC-Fipe.
+#'
+#' @param indic a character vector with economic indicators names:
+#' IGP-DI, IGP-M, INPC, IPA-DI, IPA-M, IPCA, IPCA-15, IPC-Fipe.
+#' @param start_date series initial date.
+#' Accepts ISO character formated date and \code{Date}.
+#' @param end_date series final date.
+#' Accepts ISO character formated date and \code{Date}.
+#' @param ... additional parameters to be passed to the API
+#'
+#' \code{indic} argumento must be one of these:
+#' IGP-DI, IGP-M, INPC, IPA-DI, IPA-M, IPCA, IPCA-15, IPC-Fipe.
+#' Respecting the case, blank spaces and accents.
+#'
+#' The \code{...} is to be used with API's parameters. \code{$top} to specify
+#' the maximum number of rows to be returned, this returns the \code{$top} rows,
+#' in chronological order. There is also \code{$skip} to ignore the first rows.
+#'
+#' @return
+#' A \code{data.frame} with the following ten columns: \code{date}, \code{indic},
+#' \code{smoothed}, \code{mean}, \code{median}, \code{sd},
+#' \code{coefvar}, \code{min}, \code{max}.
+#'
+#' @examples
+#' indic <- c("IPCA", "IGP-M")
+#' end_date <- "2018-06-22"
+#' x <- get_12_months_inflation_expectations(indic, end_date = end_date, `$top` = 10)
+#'
+#' @export
+get_12_months_inflation_expectations <- function(indic, start_date = NULL, end_date = NULL, ...) {
+  valid_indic = c("IGP-DI",
+                  "IGP-M",
+                  "INPC",
+                  "IPA-DI",
+                  "IPA-M",
+                  "IPCA",
+                  "IPCA-15",
+                  "IPC-Fipe")
+
+  check_indic <- indic %in% valid_indic
+  if (!all(check_indic))
+    stop("Invalid indic argument: ", paste(indic[!check_indic], collapse = ", "))
+
+  url <- twelve_months_inflation_expectations_url(indic, start_date, end_date, ...)
+
+  res <- httr::GET(url)
+
+  text_ <- httr::content(res, as = "text")
+
+  data_ <- jsonlite::fromJSON(text_)
+
+  df_ <- tibble::as_tibble(data_$value)
+  names(df_) <- c("indic", "date", "smoothed", "mean", "median", "sd", "coefvar", "min", "max")
+  df_$date <- as.Date(df_$date)
+  df_
+}
+
+twelve_months_inflation_expectations_url <- function(indic, start_date, end_date, ...) {
+  indic_filter <- paste(sprintf("Indicador eq '%s'", indic), collapse = " or ")
+  indic_filter <- paste0("(", indic_filter, ")")
+
+  sd_filter <- if (!is.null(start_date)) sprintf("Data ge '%s'", start_date) else NULL
+
+  ed_filter <- if (!is.null(end_date)) sprintf("Data le '%s'", end_date) else NULL
+
+  filter__ <- paste(c(indic_filter, sd_filter, ed_filter), collapse = " and ")
+
+  httr::modify_url("https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativasMercadoInflacao12Meses",
+                   query = list(`$filter` = filter__,
+                                `$format` = "application/json",
+                                `$orderby` = "Data desc",
+                                `$select` = "Indicador,Data,Suavizada,Media,Mediana,DesvioPadrao,CoeficienteVariacao,Minimo,Maximo", ...))
+}
+
