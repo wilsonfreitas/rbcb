d1d283ff782a89bc8dda4a5b003d602b22472cc8
wilsonfreitas <wilson.freitas@gmail.com>
2018-06-29 07:11:51 -0300

    Helpers implemented
    Bid and Ask helpers to extract information from currency time series

--- R/currency_helpers.R
+++ R/currency_helpers.R
@@ -1,18 +1,36 @@
+#' Helpers to access time series columns
+#'
+#' Extraxt bid and ask data from time series objects.
+#'
+#' @param x time series objects containing data to be extracted
+#'
+#' @name helpers
+#' @examples
+#' x <- get_currency("EUR", "2018-06-22", "2018-06-28")
+#' Bid(x)
+#' Ask(x)
+NULL
 
+#' @rdname helpers
 #' @export
 Bid <- function(x, ...) UseMethod("Bid")
 
+#' @rdname helpers
 #' @export
 Bid.data.frame <- function(x) subset(x, select = c('date', 'bid'))
 
+#' @rdname helpers
 #' @export
 Bid.xts <- function(x) x[,"bid"]
 
+#' @rdname helpers
 #' @export
 Ask <- function(x, ...) UseMethod("Ask")
 
+#' @rdname helpers
 #' @export
 Ask.data.frame <- function(x) subset(x, select = c('date', 'ask'))
 
+#' @rdname helpers
 #' @export
 Ask.xts <- function(x) x[,"ask"]
