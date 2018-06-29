9a88a8c8d42d0f2cd5a481a82dfd9e0f1ae55d91
wilsonfreitas <wilson.freitas@gmail.com>
2018-06-29 08:10:12 -0300

    Added ... to generics (S3 consistency)
    Also documented ... argument.

--- R/currency_helpers.R
+++ R/currency_helpers.R
@@ -3,6 +3,7 @@
 #' Extraxt bid and ask data from time series objects.
 #'
 #' @param x time series objects containing data to be extracted
+#' @param ... additional arguments
 #'
 #' @name helpers
 #' @examples
@@ -17,11 +18,11 @@ Bid <- function(x, ...) UseMethod("Bid")
 
 #' @rdname helpers
 #' @export
-Bid.data.frame <- function(x) subset(x, select = c('date', 'bid'))
+Bid.data.frame <- function(x, ...) subset(x, select = c('date', 'bid'))
 
 #' @rdname helpers
 #' @export
-Bid.xts <- function(x) x[,"bid"]
+Bid.xts <- function(x, ...) x[,"bid"]
 
 #' @rdname helpers
 #' @export
@@ -29,8 +30,8 @@ Ask <- function(x, ...) UseMethod("Ask")
 
 #' @rdname helpers
 #' @export
-Ask.data.frame <- function(x) subset(x, select = c('date', 'ask'))
+Ask.data.frame <- function(x, ...) subset(x, select = c('date', 'ask'))
 
 #' @rdname helpers
 #' @export
-Ask.xts <- function(x) x[,"ask"]
+Ask.xts <- function(x, ...) x[,"ask"]
