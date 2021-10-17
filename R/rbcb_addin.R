afab447a2b72e42be2ef0c9a79b6c1bfa8b510c4
wilsonfreitas <wilson.freitas@gmail.com>
2021-10-17 18:38:36 -0300

    New function addin created: rbcb_search
    rbcb_search function starts a RStudio Gadget to work as an interface to CKAN search API.
    
    The old function search_series has been removed.
    
    NAMESPACE hsa also been update and docs generated.

---
+++ R/rbcb_addin.R
@@ -0,0 +1,74 @@
+
+# library(httr)
+# library(shiny)
+# library(miniUI)
+
+#' rbcb Search
+#'
+#' @description `rbcb_search()` opens an [RStudio
+#'   gadget](https://shiny.rstudio.com/articles/gadgets.html) and
+#'   [addin](http://rstudio.github.io/rstudioaddins/) that allows you to query
+#'   for specific terms and see a suitable rbcb command to fetch the
+#'   desired data.
+#'
+#' @export
+rbcb_search <- function() {
+  ui <- miniUI::miniPage(
+    miniUI::gadgetTitleBar("rbcb search"),
+    miniUI::miniContentPanel(
+      shiny::textInput("q", "Search:"),
+      shiny::numericInput("offset", "Offset:", value = 0, min = 0, step = 10),
+      shiny::htmlOutput("page_count"),
+      shiny::tags$hr(),
+      shiny::tableOutput("results")
+    )
+  )
+
+  server <- function(input, output, session) {
+    page_count <- shiny::reactiveVal(0)
+
+    query_result <- shiny::reactive({
+      url <- parse_url("https://dadosabertos.bcb.gov.br/api/search/dataset")
+      url$query <- list(q = input$q, offset = input$offset, all_fields = 1)
+      res <- GET(url)
+      data <- res |>
+        content(as = "text") |>
+        jsonlite::fromJSON()
+      page_count(data$count)
+      data$results
+    })
+
+    query_codes <- shiny::reactive({
+      x <- query_result()
+      # x$results$res_name
+      sapply(x$res_name, function(.x) {
+        ix <- which(grepl("json_serie-sgs-\\d+", .x))
+        if (length(ix)) {
+          as.numeric(sub("json_serie-sgs-", "", .x[ix]))
+        } else {
+          NA_integer_
+        }
+      })
+    })
+
+    output$page_count <- shiny::renderUI({
+      shiny::tags$strong(paste0(page_count(), " results"))
+    })
+
+    output$results <- shiny::renderTable({
+      df <- query_result()
+      cmds <- sapply(query_codes(), function(x) {
+        if (!is.na(x)) {
+          paste0("rbcb::get_series(", x, ")")
+        } else {
+          "NA"
+        }
+      })
+      data.frame(`Result Title` = df$title, Command = cmds, check.names = FALSE)
+    })
+  }
+
+  app <- shiny::shinyApp(ui, server, options = list(quiet = TRUE))
+  shiny::runGadget(app, viewer = shiny::dialogViewer("rbcb search"))
+}
+
--- tests/testthat/test-search.R
+++
@@ -1,9 +0,0 @@
-
-context('Search series')
-
-test_that('should check if encoding is ok', {
-  x <- search_series("IPCA")
-
-  expect_equal(x[[1]][[1]]$title,
-               "Índice de Preços ao Consumidor-Amplo (IPCA) - Não comercializáveis")
-})
