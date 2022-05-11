2d64a20850f1b7a957f4cea21364367d218cba2e
WILSON NASCIMENTO DE FREITAS <WNF@modal.net.br>
2022-05-11 19:38:06 -0300

    Checking for status-code == 200 in http_getter and http_postter

--- R/http_util.R
+++ R/http_util.R
@@ -1,15 +1,21 @@
 
 http_getter <- function(url, verbose = getOption("rbcb_verbose", default = FALSE)) {
-  httr::GET(
+  res <- httr::GET(
     url = url,
     config = list(),
     if (verbose) httr::verbose(),
     httr::add_headers("User-Agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36")
   )
+  if (httr::status_code(res) != 200) {
+    msg <- sprintf("Request error %s\n", httr::status_code(res))
+    stop(msg, "URL:", url)
+  } else {
+    res
+  }
 }
 
 http_poster <- function(url, body, encode = "form", verbose = getOption("rbcb_verbose", default = FALSE)) {
-  httr::POST(
+  res <- httr::POST(
     url = url,
     config = list(),
     body = body,
@@ -17,6 +23,12 @@ http_poster <- function(url, body, encode = "form", verbose = getOption("rbcb_ve
     if (verbose) httr::verbose(),
     httr::add_headers("User-Agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36")
   )
+  if (httr::status_code(res) != 200) {
+    msg <- sprintf("Request error %s\n", httr::status_code(res))
+    stop(msg, "URL:", url)
+  } else {
+    res
+  }
 }
 
 http_gettext <- function(res, encoding = "UTF-8", as = "raw") {
